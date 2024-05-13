d4ca48241d52523ade4a83f0294e8b910f93e9e2
Matheus23dev <153209938+Matheus23dev@users.noreply.github.com>
2024-05-13 20:15:52 -0300

    Add files via upload

---
+++ Quiz espanhol/js/questions.js
@@ -0,0 +1,115 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Maria va a  aprender a reparar autos en el 'taller' de mecánica.",
+    answer: "Oficina",
+    options: [
+      "talher",
+      "colher",
+      "loja",
+      "Oficina"
+    ]
+  },
+    {
+    numb: 2,
+    question:"Después de correr, Pedro tiene sed y bebe agua de un 'vaso'.",
+    answer: "Copo",
+    options: [
+      "Vaso",
+      "Copo",
+      "Jarra",
+      "Garrafa"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Después de afeitarse, Juan ya no tiene 'vello' en la barba.",
+    answer: "Pelo",
+    options: [
+      "Velho",
+      "vela",
+      "Mancha",
+      "Pelo"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Para el desayuno, Sofia toma café en una 'taza' grande.",
+    answer: "Xícara",
+    options: [
+      "copo",
+      "Taça",
+      "Garrafa",
+      "Xícara"
+    ]
+  },
+    {
+    numb: 5,
+    question: "El perro callejero es muy 'vago', siempre duerme en la acera.",
+    answer: "Vadio",
+    options: [
+      "Vadio",
+      "veloz",
+      "Vago",
+      "Vaga"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "Marta necesita medir la 'tasa' de  crecimiento de las plantas en el experimento.",
+    answer: "Taxa",
+    options: [
+      "altura",
+      "Largura",
+      "tampa",
+      "Taxa"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Marta pone una 'tapa' en la sartén para cocinar los huevos.",
+    answer: "Tampa",
+    options: [
+      "Prato",
+      "Pano",
+      "estampa",
+      "Tampa"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Los niños se lavantan 'temprano' para ir a la escuela.",
+    answer: "Cedo",
+    options: [
+      "Tarde",
+      "Tempo",
+      "Cedo",
+      "Cansado"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Juan escribe con la mano derecha, no es 'zurdo'.",
+    answer: "Canhoto",
+    options: [
+      "Surdo",
+      "fraco",
+      "Destro",
+      "Canhoto"
+    ]
+  },
+  {
+    numb: 10,
+    question: "El 'vacio' en el tanque de gasolina indica que necesitas llenario.",
+    answer: "Vago",
+    options: [
+      "Lugar",
+      "Vaga",
+      "tampa",
+      "Vago"
+    ]
+  },
+
+];
